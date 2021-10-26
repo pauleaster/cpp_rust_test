8497dbd7a7b5130c105cb42e2df2d4a8e7d6ba21
Paul Easter <paul.john.easter@gmail.com>
2021-10-26 20:24:30 +1100

    Moved out code to print_floats so that this can subsequently be moved to a library for replacement by rust code

--- hello.c
+++ hello.c
@@ -2,27 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+int print_floats(char * string_of_floats) {
 
-
-int main(void)
-{
-    char * str  = "334.5 666.5 -123 3.1415926536";
     char * pEnd, * pStart;
-    printf("hello, World!\n");
     float f1;
-    pStart = str;
-    pEnd = str;
+    pStart = string_of_floats;
+    pEnd = string_of_floats;
+    int len = strlen(string_of_floats);
+    int char_size = sizeof(char);
     while (1) {
         printf("Before: %p %p\n",pStart,pEnd);
         f1 = strtof(pStart,&pEnd); // if no result then pStart == pEnd
         if (pEnd == pStart) {
-            break;
+            printf("Breaking, index=%d, length = %d\n",(pEnd - string_of_floats)/char_size,len);
+            return (1);
         }
         printf("%0.5f\n",f1);
         printf("After: %p %p\n\n",pStart,pEnd);
         pStart = pEnd; // Move the starting position to the end of the last read
 
     }
+}
 
-    return 0;
+
+
+int main(void)
+{
+    char * str  = "334.5 666.5 -123 3.1415926536";
+    printf("hello, World!\n");
+
+    return print_floats(str);
 }
